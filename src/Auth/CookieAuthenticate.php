15521885ed778abbdce6a2675318e0346be68dfc
Justin Atack <justinatack@gmail.com>
2016-08-23 15:53:53 +1000

    Initial CookieAuththenticate commit
    Not ready for production

---
+++ src/Auth/CookieAuthenticate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JustinAtack\Authenticate\Auth;
+
+use Cake\Auth\BaseAuthenticate;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\ORM\TableRegistry;
+
+class CookieAuthenticate extends BaseAuthenticate
+{
+    // code here
+}
